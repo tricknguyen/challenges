81e3159d4b5a5c2986184e0d07a5fce82fe541b1
Nguyen Anh Vu <tricknguyen2607@gmail.com>
2024-09-12 21:04:15 +0700

    New challenge

---
+++ 2824/resolve.ts
@@ -0,0 +1,13 @@
+//https://leetcode.com/problems/count-pairs-whose-sum-is-less-than-target/description/
+
+function countPairs(nums: number[], target: number): number {
+    let count = 0;
+    for (let i=0; i<nums.length; i++) {
+        for (let j=i+1; j < nums.length; j++) {
+            if (nums[i] + nums[j] < target) {
+                count++;
+            }
+        }
+    }
+    return count;
+};
