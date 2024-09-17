73455e903e2070ba462e410eb902b1436cef492f
Nguyen Anh Vu <tricknguyen2607@gmail.com>
2024-09-17 22:16:11 +0700

    Add challenge blin75 🤠

---
+++ leetcode/blind75/1/resolved.ts
@@ -0,0 +1,13 @@
+//https://leetcode.com/problems/two-sum/
+
+function twoSum(nums: number[], target: number): number[] {
+    let result: Array<number> = [];
+    for (let i=0; i<nums.length; i++) {
+        for (let j =i+1; j<nums.length; j++) {
+            if ((nums[i] + nums[j]) === target) {
+                result = [i, j];
+            }
+        }
+    }
+    return result;
+};
