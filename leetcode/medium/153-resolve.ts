72dd84ba074c96036d72ef6d57187179bfdd4d27
Nguyen Anh Vu <tricknguyen2607@gmail.com>
2024-09-25 07:38:01 +0700

    New solution for find minimum in rotated sorted array 🫠

--- leetcode/medium/153-resolve.ts
+++ leetcode/medium/153-resolve.ts
@@ -1,22 +1,16 @@
-// https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/description/
+//https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/description/
 
 function findMin(nums: number[]): number {
     let left = 0;
     let right = nums.length - 1;
 
     while (left < right) {
-        const mid = Math.floor((left + right) / 2);
-
-        if (nums[mid] > nums[right]) {
-            // The minimum element is in the right half
-            left = mid + 1;
+        const middle = Math.floor((right+left) / 2);
+        if (nums[middle] > nums[right]) {
+            left = middle + 1;
         } else {
-            // The minimum element is in the left   
-            right = mid;
+            right = middle;
         }
     }
-
     return nums[left];
 };
-
-console.log(findMin([3, 4, 5, 1, 2]));
