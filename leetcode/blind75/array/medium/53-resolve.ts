f86acddaf9a5625d9594f374881ee25e202d4610
Nguyen Anh Vu <gakistal12@gmail.com>
2024-09-30 20:54:45 +0700

    Add new solution to maximum subarray challenge 🛺

--- leetcode/blind75/array/medium/53-resolve.ts
+++ leetcode/blind75/array/medium/53-resolve.ts
@@ -18,3 +18,18 @@ function maxSubArray(nums: number[]): number {
 
     return result;
 };
+
+
+//// new solution
+function maxSubArray2(nums: number[]): number {
+    let ans = -10000;
+    let curSum = 0;
+    nums.forEach(num=>{
+        curSum += num;
+        if(curSum > ans)
+            ans = curSum;
+        if(curSum < 0)
+            curSum = 0;
+    })
+    return ans;
+};
