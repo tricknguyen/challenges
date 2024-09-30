bc814f989e1285dfbe3d66ded5254b5856f7c6a7
Nguyen Anh Vu <gakistal12@gmail.com>
2024-09-30 20:45:33 +0700

    Current solution to maximum subarray 🚓

--- leetcode/blind75/array/medium/53-resolve.ts
+++ leetcode/blind75/array/medium/53-resolve.ts
@@ -1,5 +1,17 @@
 //https://leetcode.com/problems/maximum-subarray/description/
 
 function maxSubArray(nums: number[]): number {
-    
+    let result = nums[0];
+
+    for (let i = 0; i< nums.length; i++) {
+        let currentSum = nums[i];
+        for (let j = i+1; j <nums.length; j++) {
+            currentSum = currentSum + nums[j];
+            if (currentSum > result) {
+                result = currentSum;
+            }
+        }
+    }
+
+    return result;
 };
