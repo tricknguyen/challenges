61d5d90d064bc2d3f8d5e87c7e1fcbb7703d8cc8
Nguyen Anh Vu <gakistal12@gmail.com>
2024-10-01 22:24:42 +0700

    Add temporarily solution to challenge product of array except self 🍣

--- leetcode/blind75/array/medium/238-resolve.ts
+++ leetcode/blind75/array/medium/238-resolve.ts
@@ -1,3 +1,13 @@
+//https://leetcode.com/problems/product-of-array-except-self/description/
+//The solution not enough good about performance
 function productExceptSelf(nums: number[]): number[] {
-    return [0];
+    const result: Array<number> = [];
+    for (let i=0; i< nums.length; i++) {
+        const subNums = [...nums.slice(0,i), ...nums.slice(i+1)];
+        const multi = subNums.reduce((prevValue,curValue) => {
+            return prevValue * curValue
+        },1);
+        result.push(multi);
+    }
+    return result;
 };
